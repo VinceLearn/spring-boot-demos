c28d424ee3bb4693341453ad249096197f50465d
zhourl <zhouronglv@gmail.com>
2018-03-31 17:16:37 +0800

    init

---
+++ spring-boot-web/src/main/java/xin/keepmoving/web/config/RedisConfig.java
@@ -0,0 +1,72 @@
+package xin.keepmoving.web.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+
+import static org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig;
+import static java.util.Collections.singletonMap;
+
+/**
+ * @author: Zhourl
+ * @description: spring-boot-demos
+ * @version: 1.0
+ * @date: 2018/3/31
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig {
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheManager cm = RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultCacheConfig())
+                .withInitialCacheConfigurations(singletonMap("predefined",
+                        defaultCacheConfig().disableCachingNullValues()))
+                .transactionAware()
+                .build();
+        return cm;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
---
+++ spring-boot-web/src/test/java/xin/keepmoving/web/cache/RedisTests.java
@@ -0,0 +1,53 @@
+package xin.keepmoving.web.cache;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import xin.keepmoving.web.SpringBootWebApplication;
+import xin.keepmoving.web.domain.User;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: Zhourl
+ * @description: spring-boot-demos
+ * @version: 1.0
+ * @date: 2018/3/31
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringBootWebApplication.class)
+public class RedisTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test() {
+        stringRedisTemplate.opsForValue().set("bbb", "222");
+        Assert.assertEquals("222", stringRedisTemplate.opsForValue().get("bbb"));
+    }
+
+    @Test
+    public void testObj() throws InterruptedException {
+        User user = new User("alijj", "123456", "alijj@163.com");
+        ValueOperations<String, User> operations = redisTemplate.opsForValue();
+        operations.set("xin.keepmoving", user);
+        operations.set("xin.keepmoving.f", user, 1, TimeUnit.SECONDS);
+        Thread.sleep(1000);
+        Boolean exists = redisTemplate.hasKey("xin.keepmoving.f");
+        if (exists) {
+            System.out.println("exists is true");
+        } else {
+            System.out.println("exists is false");
+        }
+        Assert.assertEquals("alijj", operations.get("xin.keepmoving").getUsername());
+    }
+}
